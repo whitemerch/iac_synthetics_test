b837b8122e50b507f3b8ce1f434eae5c9510779a
Chakib Hamie <chakib.hamie@datadoghq.com>
2025-11-19 09:42:54 +0100

    Simplify SQS configuration to single unencrypted queue

--- sqs_misconfigurations/sqs_misconfigurations.tf
+++ sqs_misconfigurations/sqs_misconfigurations.tf
@@ -9,12 +9,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "queues" {
-  description = "Queue names to create or import (unencrypted on apply)."
-  type        = map(string)
-  default     = {"queue-a": "iac-misconfig-queue-a", "queue-b": "iac-misconfig-queue-b"}
-}
-
 locals {
   delay_seconds             = 0
   max_message_size          = 262144
@@ -22,18 +16,8 @@ locals {
   receive_wait_time_seconds = 10
 }
 
-# Optional: import existing queues with these names (Terraform 1.5+)
-# Comment out this block if you only want to create new queues.
-# import {
-#   for_each = var.queues
-#   id       = "https://sqs.us-east-1.amazonaws.com/486234852809/${each.value}"
-#   to       = aws_sqs_queue.sqs_queues[each.value]
-# }
-
-resource "aws_sqs_queue" "sqs_queues" {
-  for_each = var.queues
-
-  name                      = each.value
+resource "aws_sqs_queue" "unencrypted_queue" {
+  name                      = "iac-test-unencrypted-queue"
   delay_seconds             = local.delay_seconds
   max_message_size          = local.max_message_size
   message_retention_seconds = local.message_retention_seconds
@@ -43,6 +27,6 @@ resource "aws_sqs_queue" "sqs_queues" {
   sqs_managed_sse_enabled = false
 }
 
-output "queue_urls" {
-  value = { for k, q in aws_sqs_queue.sqs_queues : k => q.url }
+output "queue_url" {
+  value = aws_sqs_queue.unencrypted_queue.url
 }
