84b7a5a6bea83a29f26103e9edad8ed4c51ec6fa
Chakib Hamie <chakib.hamie@datadoghq.com>
2026-02-17 08:47:43 +0100

    Add MailgunApiKey class to print API key

---
+++ secrets.java
@@ -0,0 +1,6 @@
+public class MailgunApiKey {
+    public static void main(String[] args){
+        String key = "mailgun_api_key=key-1644b44d50bcc02f6f789aaf324f82ec";
+        System.out.println(key);
+    }
+}
